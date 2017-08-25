d0193b86259f66bb22b10e64ebe407521a4db7c5
William Forde <willforde@gmail.com>
2017-08-25 04:01:06 +0100

    update

---
+++ README.rst
@@ -0,0 +1,16 @@
+.. image:: https://api.codacy.com/project/badge/Grade/b840d9b5a9a341d18c2c6161eb653954
+    :target: https://www.codacy.com/app/willforde/plugin.video.metalvideo?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=willforde/plugin.video.metalvideo&amp;utm_campaign=Badge_Grade
+
+Metalvideo
+==========
+.. image:: resources/icon.png
+
+This is a kodi video add-on for Heavy Metal Music Videos.
+
+A Place for all your heavy metal music videos, With loads of categorys.
+
+The add-on is available in the official kodi repository and is available on kodi v17 "Krypton" and greater.
+
+.. image:: resources/screenshots/screenshot000.jpg
+.. image:: resources/screenshots/screenshot001.jpg
+.. image:: resources/screenshots/screenshot002.jpg
--- addon.py
+++ addon.py
@@ -1,261 +1,281 @@
-"""
-Copyright: (c) 2016 William Forde (willforde+kodi@gmail.com)
-License: GPLv3, see LICENSE for more details
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2016 William Forde (willforde+kodi@gmail.com)
+# License: GPLv3, see LICENSE for more details
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-"""
-
+from __future__ import unicode_literals
+from codequick import Route, Resolver, Listitem, utils, run
+import xbmcgui
 import re
-import urlparse
-import codequick as plugin
 
-plugin.strings.update(video_of_the_day=30104, watching_now=30105, random_video=30103,
-                      top_videos=30102, select_top=30101)
+# Localized string Constants
+VIDEO_OF_THE_DAY = 30004
+WATCHING_NOW = 30005
+TOP_VIDEOS = 30002
+SELECT_TOP = 30001
+PARTY_MODE = 589
+
+# Base url constructor
+url_constructor = utils.urljoin_partial("http://metalvideo.com")
 
 
-def fetch_query(url, key):
-    """Parses a url and return the vid parameter"""
-    href = urlparse.urlsplit(url)
-    return urlparse.parse_qs(href.query)[key][0]
+# noinspection PyUnusedLocal
+@Route.register
+def root(plugin, content_type=None):
+    """
+    :param Route plugin: The plugin parent object.
+    :param str content_type: The type of content been listed e.g. video, music. This is passed in from kodi and
+                             we have no use for it as of yet.
+    """
+    yield Listitem.recent(recent_videos)
+    yield Listitem.from_dict(plugin.localize(TOP_VIDEOS), top_videos)
+    yield Listitem.from_dict(plugin.localize(WATCHING_NOW), watching_now)
+    yield Listitem.search(video_list)
 
-
-@plugin.route("/")
-def root():
     # Fetch HTML Source
-    url = u"http://metalvideo.com/mobile/category.html"
-    html = plugin.requests_session().get(url, headers={"Cookie": "COOKIE_DEVICE=mobile", "x-max-age": 604800}).text
-    icon = plugin.get_info("icon")
-
-    # Run SpeedForce to attempt to strip Out any unneeded html tags
-    stripper = plugin.utils.ETBuilder(u"ul", attrs={u"id": u"category_listing"})
-    root_elem = stripper.run(html)
+    url = url_constructor("/mobile/category.html")
+    html = plugin.request.get(url, headers={"Cookie": "COOKIE_DEVICE=mobile"})
+    root_elem = html.parse(u"ul", attrs={"id": "category_listing"})
     for elem in root_elem.iterfind("li"):
         a_tag = elem.find("a")
-        item = plugin.ListItem()
-        item.art["thumb"] = icon
+        item = Listitem()
 
         # Set label with video count added
-        item.label = u"%s (%s)" % (a_tag.text, elem.find("span").text)
+        item.label = "%s (%s)" % (a_tag.text, elem.find("span").text)
+        item.set_callback(video_list, url=a_tag.get("href"))
+        yield item
 
-        # Fetch category
-        item.url["cat"] = fetch_query(a_tag.get("href"), "cat")
-        yield item.get(video_list)
-
-    yield plugin.ListItem.add_recent(recent_videos)
-    yield plugin.ListItem.add_item(play_video, plugin.localize("video_of_the_day"), icon, url=u"index.html")
-    yield plugin.ListItem.add_item(party_play, plugin.localize("random_video"), icon, url=u"randomizer.php")
-    yield plugin.ListItem.add_item(top_videos, plugin.localize("top_videos"), icon)
-    yield plugin.ListItem.add_item(watching_now, plugin.localize("watching_now"), icon)
-    yield plugin.ListItem.add_search(video_list, url=u"search.php?keywords=%s")
+    # Add the video items here so that show at the end of the listing
+    yield Listitem.from_dict(plugin.localize(VIDEO_OF_THE_DAY), play_video, params={"url": "index.html"})
+    yield Listitem.from_dict(plugin.localize(PARTY_MODE), party_play, params={"url": "randomizer.php"})
 
 
-@plugin.route("/recent_videos")
-def recent_videos():
+@Route.register
+def recent_videos(plugin, url="newvideos.php"):
+    """
+    :param Route plugin: The plugin parent object.
+    :param unicode url: The url resource containing recent videos.
+    """
     # Fetch HTML Source
-    url = u"http://metalvideo.com/%s" % plugin.params.get(u"url", u"newvideos.php")
-    html = plugin.requests_session().get(url).text
-
-    # Run SpeedForce to atempt to strip Out any unneeded html tags
-    stripper = plugin.utils.ETBuilder(u"div", attrs={u"id": u"browse_main"})
-    root_elem = stripper.run(html)
+    url = url_constructor(url)
+    html = plugin.request.get(url)
+    root_elem = html.parse("div", attrs={"id": "browse_main"})
     node = root_elem.find("./div[@id='newvideos_results']")[0]
     for elem in node.iterfind("./tr"):
         if not elem.attrib:
-            item = plugin.ListItem()
+            item = Listitem()
             item.art["thumb"] = elem.find(".//img").get("src")
 
             artist = elem[1].text
             track = elem[1][0][0].text
-            item.label = u"%s - %s" % (artist, track)
+            item.label = "%s - %s" % (artist, track)
             item.info["artist"] = [artist]
 
-            # Fetch video id
-            videoid = fetch_query(elem.find(".//a").get("href"), "vid")
-            item.url["videoid"] = videoid
-
-            # Add related video context item
-            item.context.related(related, videoid=videoid)
-            yield item.get(play_video)
+            url = elem.find(".//a").get("href")
+            item.context.related(related, url=url)
+            item.set_callback(play_video, url=url)
+            yield item
 
     # Fetch next page url
     next_tag = root_elem.find("./div[@class='pagination']").findall("./a")[-1]
     if next_tag.text.startswith("next"):
-        yield plugin.ListItem.add_next(url=next_tag.get("href"))
+        print next_tag.get("href")
+        yield Listitem.next_page(url=next_tag.get("href"))
 
 
-@plugin.route("/watching_now")
-def watching_now():
+@Route.register
+def watching_now(plugin):
     # Fetch HTML Source
-    url = u"http://metalvideo.com/index.html"
-    html = plugin.requests_session().get(url).text
-
-    # Run SpeedForce to atempt to strip Out any unneeded html tags
-    stripper = plugin.utils.ETBuilder(u"ul", attrs={u"id": u"mycarousel"})
-    root_elem = stripper.run(html)
+    url = url_constructor("/index.html")
+    html = plugin.request.get(url)
+    root_elem = html.parse("ul", attrs={"id": "mycarousel"})
     for elem in root_elem.iterfind("li"):
         a_tag = elem.find(".//a[@title]")
-        item = plugin.ListItem()
+        item = Listitem()
 
+        # Fetch label & thumb
         item.label = a_tag.text
         item.art["thumb"] = elem.find(".//img").get("src")
 
-        # Fetch video id from url
-        videoid = fetch_query(a_tag.get("href"), "vid")
-        item.url["videoid"] = videoid
-
-        # Add related video context item
-        item.context.related(related, videoid=videoid)
-        yield item.get(play_video)
+        url = a_tag.get("href")
+        item.context.related(related, url=url)
+        item.set_callback(play_video, url=url)
+        yield item
 
 
-@plugin.route("/topvideos")
-def top_videos():
+@Route.register
+def top_videos(plugin):
+    """:param Route plugin: The plugin parent object."""
     # Fetch HTML Source
-    url = u"http://metalvideo.com/topvideos.html"
-    html = plugin.requests_session().get(url, headers={"x-max-age": 604800}).text
+    url = url_constructor("/topvideos.html")
+    html = plugin.request.get(url)
     titles = []
     urls = []
 
     # Parse categories
-    flash = plugin.utils.ETBuilder(u"select", attrs={u"name": u"categories"})
-    root_elem = flash.run(html)
+    root_elem = html.parse("select", attrs={"name": "categories"})
     for group in root_elem.iterfind("optgroup"):
         for elem in group:
             urls.append(elem.get("value"))
-            titles.append(elem.text)
+            titles.append(elem.text.strip())
 
-    import xbmcgui
     # Display list for Selection
     dialog = xbmcgui.Dialog()
-    ret = dialog.select(plugin.localize("select_top"), titles)
+    ret = dialog.select(plugin.localize(SELECT_TOP), titles)
     if ret >= 0:
         # Fetch HTML Source
         url = urls[ret]
-        html = plugin.requests_session().get(url).text
-
-        # Parse top videos
-        flash = plugin.utils.ETBuilder(u"div", attrs={u"id": u"topvideos_results"})
-        root_elem = flash.run(html)
+        html = plugin.request.get(url)
+        root_elem = html.parse("div", attrs={"id": "topvideos_results"})
         for elem in root_elem.iterfind(".//tr"):
             if not elem.attrib:
-                item = plugin.ListItem()
+                item = Listitem()
                 a_tag = elem[3][0]
 
                 artist = elem[2].text
-                item.label = u"%s %s - %s" % (elem[0].text, artist, a_tag.text)
+                item.label = "%s %s - %s" % (elem[0].text, artist, a_tag.text)
                 item.art["thumb"] = elem.find(".//img").get("src")
-                item.info["count"] = elem[4].text.replace(u",", u"")
+                item.info["count"] = elem[4].text.replace(",", "")
                 item.info["artist"] = [artist]
 
-                # Fetch videoid
-                videoid = fetch_query(a_tag.get("href"), "vid")
-                item.url["videoid"] = videoid
-
-                # Add related video context item
-                item.context.related(related, videoid=videoid)
-                yield item.get(play_video)
+                url = a_tag.get("href")
+                item.context.related(related, url=url)
+                item.set_callback(play_video, url=url)
+                yield item
 
 
-@plugin.route("/related")
-def related():
+@Route.register
+def related(plugin, url):
+    """
+    :param Route plugin: The plugin parent object.
+    :param unicode url: The url of a video.
+    """
     # Fetch HTML Source
-    url = u"http://metalvideo.com/relatedclips.php?vid=%(videoid)s" % plugin.params
-    xml = plugin.requests_session().get(url).text
+    url = url_constructor(url)
+    html = plugin.request.get(url)
+    root_elem = html.parse("div", attrs={"id": "tabs_related"})
 
     # Parse the xml
-    import xml.etree.ElementTree as ElementTree
-    root_elem = ElementTree.fromstring(xml)
-    for elem in root_elem.iterfind(u"video"):
-        item = plugin.ListItem()
-        item.label = elem.findtext(u"title")
-        item.art["thumb"] = elem.findtext(u"thumb")
+    for elem in root_elem.iterfind(u"div"):
+        a_tag = elem.find("./a[@class='song_name']")
 
-        # Fetch video id
-        videoid = fetch_query(elem.findtext(u"url"), "vid")
-        item.url["videoid"] = videoid
+        item = Listitem()
+        item.label = a_tag.text
+        item.art["thumb"] = elem.find("./a/img").get("src")
 
-        # Add related video context item
-        item.context.related(related, videoid=videoid)
-        yield item.get(play_video)
+        url = a_tag.get("href")
+        item.context.related(related, url=url)
+        item.set_callback(play_video, url=url)
+        yield item
 
 
-@plugin.route("/videos")
-def video_list():
-    # Fetch HTML Source
-    if u"url" in plugin.params:
-        url = u"http://metalvideo.com/%(url)s" % plugin.params
+@Route.register
+def video_list(plugin, url=None, cat=None, search_query=None):
+    """
+    :param Route plugin: The plugin parent object.
+    :param unicode url: The url resource containing lists of videos or next page.
+    :param unicode cat: A category url e.g. Alternative, Folk Metal.
+    :param unicode search_query: The video search term to use for searching.
+    """
+    if search_query:
+        url = url_constructor("search.php?keywords=%s&btn=Search" % search_query)
+    elif cat:
+        sortby = (u"date.html", u"artist.html", u"rating.html", u"views.html")[plugin.setting.get_int("sort")]
+        base, _ = url_constructor(cat).rsplit("-", 1)
+        url = "-".join((base, sortby))
     else:
-        sortby = {0: u"date", 1: u"artist", 2: u"rating", 3: u"views"}[plugin.get_setting("sort")]
-        url = u"http://metalvideo.com/category.php?cat=%s&sortby=%s" % (plugin.params["cat"], sortby)
-    html = plugin.requests_session().get(url).text
+        url = url_constructor(url)
 
-    # Run SpeedForce to atempt to strip Out any unneeded html tags
-    stripper = plugin.utils.ETBuilder(u"div", attrs={u"id": u"browse_main"})
-    root_elem = stripper.run(html)
+    html = plugin.request.get(url)
+    root_elem = html.parse("div", attrs={"id": "browse_main"})
     for elem in root_elem.iterfind(u".//div[@class='video_i']"):
-        a_tag = elem.findall("a")[-1]
-        item = plugin.ListItem()
+        item = Listitem()
         item.art["thumb"] = elem.find(".//img").get("src")
 
+        # Extract url and remove first 'a' tag section
+        # This makes it easir to extract 'artist' and 'song' name later
+        a_tag = elem.find("a")
+        url = a_tag.get("href")
+        elem.remove(a_tag)
+
         # Fetch title
-        span_tags = tuple([node.text for node in a_tag.findall("span")])
+        span_tags = tuple(node.text for node in elem.findall(".//span"))
         item.label = "%s - %s" % span_tags
         item.info["artist"] = [span_tags[0]]
 
-        # Fetch videoid
-        videoid = fetch_query(a_tag.get("href"), "vid")
-        item.url["videoid"] = videoid
-
         # Add related video context item
-        item.context.related(related, videoid=videoid)
-        yield item.get(play_video)
+        item.context.related(related, url=url)
+        item.set_callback(play_video, url=url)
+        yield item
 
     # Fetch next page url
     next_tag = root_elem.find(".//div[@class='pagination']").findall("./a")[-1]
     if next_tag.text.startswith("next"):
-        yield plugin.ListItem.add_next(url=next_tag.get("href"))
+        yield Listitem.next_page(url=next_tag.get("href"))
 
 
-@plugin.resolve("/playvideo")
-def play_video():
-    # Fetch HTML Source
-    if u"url" in plugin.params:
-        url = u"http://metalvideo.com/%(url)s" % plugin.params
+@Resolver.register
+def play_video(plugin, url):
+    """
+    :param Resolver plugin: The plugin parent object.
+    :param unicode url: The url of a video.
+    :returns: A playable video url.
+    """
+    # Attemp to find url using extract_source(YTDL) first
+    url = url_constructor(url)
+    source_url = plugin.extract_source(url)
+    if source_url:
+        return source_url
+
+    # Fallback to search for direct file
+    html = plugin.request.get(url).text
+    search_regx = 'file:\s+\'(\S+?)\''
+    match = re.findall(search_regx, html)
+    if match:
+        return match[0]
+
+
+@Resolver.register
+def party_play(plugin, url):
+    """
+    :param Resolver plugin: The plugin parent object.
+    :param unicode url: The url to a video.
+    :return: A playlist with the first item been a playable video url and the seconde been a callback url that
+             will fetch the next video url to play.
+    """
+    # Attempt to fetch a video url 3 times
+    attempts = 0
+    while attempts < 3:
+        try:
+            url = play_video(plugin, url)
+        except Exception as e:
+            # Raise the Exception if we are on the last run of the loop
+            if attempts == 2:
+                raise e
+        else:
+            if url:
+                # Break from loop when we have a url
+                return plugin.create_loopback(url)
+
+        # Increment attempts counter
+        attempts += 1
     else:
-        url = u"http://metalvideo.com/musicvideo.php?vid=%(videoid)s" % plugin.params
-    html = plugin.requests_session().get(url).text
-
-    # Look for Youtube Video First
-    try:
-        search_regx = 'src="(http://www.youtube.com/embed/\S+?)"|file:\s+\'(\S+?)\''
-        match = re.findall(search_regx, html)[0]
-        url = filter(None, match)[0]
-    except IndexError:
-        pass
-    else:
-        if u"metalvideo.com" in url:
-            return url
-        elif u"youtube.com" in url:
-            return plugin.youtube.parse_url(url)
-
-
-@plugin.resolve("/partymode")
-def party_play():
-    url = play_video()
-    return party_play.create_loopback(url)
+        # All 3 attemps failed to resolve a url
+        return None
 
 
 # Initiate Startup
 if __name__ == "__main__":
-    plugin.run(True)
+    run()
