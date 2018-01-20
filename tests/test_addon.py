98f50be1bd1190b66a7d8e2ed90674bdad16e2a2
William Forde <willforde@gmail.com>
2018-01-20 07:14:37 +0000

    finished unittests

--- tests/test_addon.py
+++ tests/test_addon.py
@@ -1,4 +1,4 @@
-from addondev import testing
+from addondev import testing, plugin_data
 import unittest
 
 # Testing specific imports
@@ -8,6 +8,10 @@ import addon
 class Tester(unittest.TestCase):
     def test_root(self):
         data = addon.root.test("video")
+        self.assertGreaterEqual(len(data), 33)
+
+    def test_root_no_content_type(self):
+        data = addon.root.test()
         self.assertGreaterEqual(len(data), 33)
 
     def test_recent(self):
@@ -39,11 +43,44 @@ class Tester(unittest.TestCase):
         self.assertGreaterEqual(len(data), 25)
 
     def test_top_all(self):
-        testing.mock_select_dialog(0)
-        data = addon.top_videos.test()
+        with testing.mock_select_dialog(0):
+            data = addon.top_videos.test()
         self.assertGreaterEqual(len(data), 50)
 
     def test_top_converts(self):
-        testing.mock_select_dialog(5)
-        data = addon.top_videos.test()
+        with testing.mock_select_dialog(5):
+            data = addon.top_videos.test()
         self.assertGreaterEqual(len(data), 50)
+
+    def test_top_skip(self):
+        with testing.mock_select_dialog(-1):
+            data = addon.top_videos.test()
+        self.assertEqual(len(data), 1)
+        self.assertFalse(data[0])
+
+    def test_video_decode_youtube_iframe(self):
+        data = addon.play_video.test(url="/megadeth/holy-wars-the-punishment-due-video_4b77ac7ad.html")
+        self.assertEqual(data, "plugin://plugin.video.youtube/play/?video_id=9d4ui9q7eDM")
+
+    def test_video_decode_youtube_embed(self):
+        data = addon.play_video.test(url="/rammstein/du-hast-video_126711d81.html")
+        self.assertEqual(data, "plugin://plugin.video.youtube/play/?video_id=W3q8Od5qJio")
+
+    def test_video_decode_clips(self):
+        data = addon.play_video.test(url="/buckcherry/crazy-bitch-video_6bf13e2a4.html")
+        self.assertEqual(data, "http://metalvideo.com/videos.php?vid=6bf13e2a4")
+
+    def test_party_play(self):
+        data = addon.party_play.test(url="/sanctification/storm-video_44f0b3076.html")
+        self.assertEqual(data["path"], "plugin://plugin.video.youtube/play/?video_id=tUhkkYoeQK0")
+
+        self.assertEqual(len(plugin_data["playlist"]), 2)
+        self.assertEqual(plugin_data["playlist"][1].getLabel(), "_loopback_ - ")
+
+    def test_party_play_fail(self):
+        data = addon.party_play.test(url="http://metalvideo.com/contact_us.html")
+        self.assertIsNone(data)
+
+    def test_party_play_error(self):
+        with self.assertRaises(Exception):
+            addon.party_play.test(url="http://metalvideo.ie")
