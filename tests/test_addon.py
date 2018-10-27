767092b8704a0ebc42e21c163441e5f897d222d9
willforde <willforde@gmail.com>
2018-10-27 02:04:20 +0100

    update unittests

--- tests/test_addon.py
+++ tests/test_addon.py
@@ -62,34 +62,17 @@ class Tester(unittest.TestCase):
             data = addon.top_videos.test()
         self.assertFalse(data)
 
-    # @unittest.skipUnless(framework_version >= (0, 9, 2), "Only work on v0.9.2 and up of codequick")
-    # def test_video_decode_youtube_iframe(self):
-    #     data = addon.play_video.test(url="/megadeth/holy-wars-the-punishment-due-video_4b77ac7ad.html")
-    #     self.assertEqual(data, "plugin://plugin.video.youtube/play/?video_id=9d4ui9q7eDM")
-    #
-    # @unittest.skipUnless(framework_version >= (0, 9, 2), "Only work on v0.9.2 and up of codequick")
-    # def test_video_decode_youtube_embed(self):
-    #     data = addon.play_video.test(url="/rammstein/du-hast-video_126711d81.html")
-    #     self.assertEqual(data, "plugin://plugin.video.youtube/play/?video_id=W3q8Od5qJio")
-    #
-    # @unittest.skipUnless(framework_version >= (0, 9, 2), "Only work on v0.9.2 and up of codequick")
-    # def test_video_decode_clips(self):
-    #     data = addon.play_video.test(url="/buckcherry/crazy-bitch-video_6bf13e2a4.html")
-    #     self.assertEqual(data, "http://metalvideo.com/videos.php?vid=6bf13e2a4")
-    #
-    # @unittest.skipUnless(framework_version >= (0, 9, 2), "Only work on v0.9.2 and up of codequick")
-    # def test_party_play(self):
-    #     data = addon.party_play.test(url="/sanctification/storm-video_44f0b3076.html")
-    #     self.assertEqual(data["path"], "plugin://plugin.video.youtube/play/?video_id=tUhkkYoeQK0")
-    #
-    #     self.assertEqual(len(plugin_data["playlist"]), 2)
-    #     self.assertEqual(plugin_data["playlist"][1].getLabel(), "_loopback_ - ")
-    #
-    # @unittest.skipUnless(framework_version >= (0, 9, 2), "Only work on v0.9.2 and up of codequick")
-    # def test_party_play_fail(self):
-    #     data = addon.party_play.test(url="http://metalvideo.com/contact_us.html")
-    #     self.assertIsNone(data)
-    #
-    # def test_party_play_error(self):
-    #     with self.assertRaises(Exception):
-    #         addon.party_play.test(url="http://metalvideo.ie")
+    def test_party_play(self):
+        data = addon.party_play.test(url="/deathrite-obscure-shades_7b43ebd26.html")
+        self.assertEqual("plugin://plugin.video.youtube/play/?video_id=axi8w_vgQf0", data["path"])
+
+        self.assertEqual(len(plugin_data["playlist"]), 2)
+        self.assertEqual(plugin_data["playlist"][1].getLabel(), "_loopback_ - ")
+
+    def test_party_play_fail(self):
+        data = addon.party_play.test(url="http://metalvideo.com/contact_us.html")
+        self.assertIsNone(data)
+
+    def test_party_play_error(self):
+        with self.assertRaises(Exception):
+            addon.party_play.test(url="http://metalvideo.ie")
